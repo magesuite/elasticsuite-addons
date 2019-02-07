a883ec1efbaf6bd42647c6b0f9d78a228582cbb2
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2019-02-07 08:05:54 +0000

    Display searchbox field in attribute

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -9,18 +9,22 @@ class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
         $setup->startSetup();
 
         if (version_compare($context->getVersion(), '1.0.1', '<')) {
-            $this->addSliderDisplayModeCollumnToEavAttribute($setup);
+            $this->addSliderDisplayModeColumnToEavAttribute($setup);
+        }
+
+        if (version_compare($context->getVersion(), '1.0.2', '<')) {
+            $this->addDisplaySearchboxColumnToEavAttribute($setup);
         }
 
         $setup->endSetup();
     }
 
-    protected function addSliderDisplayModeCollumnToEavAttribute($setup)
+    protected function addSliderDisplayModeColumnToEavAttribute($setup)
     {
         $field = 'slider_display_mode';
         $table = $setup->getTable('catalog_eav_attribute');
 
-        if($setup->getConnection()->tableColumnExists($table, $field) !== false){
+        if($setup->getConnection()->tableColumnExists($table, $field)){
             return;
         }
 
@@ -33,6 +37,28 @@ class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
                 'default'  => null,
                 'length'   => 20,
                 'comment'  => 'Slider Display Mode',
+            ]
+        );
+    }
+
+    protected function addDisplaySearchboxColumnToEavAttribute($setup)
+    {
+        $field = 'display_searchbox';
+        $table = $setup->getTable('catalog_eav_attribute');
+
+        if($setup->getConnection()->tableColumnExists($table, $field)){
+            return;
+        }
+
+        $setup->getConnection()->addColumn(
+            $table,
+            $field,
+            [
+                'type'     => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                'nullable' => true,
+                'default'  => null,
+                'length'   => 20,
+                'comment'  => 'Display Search Box',
             ]
         );
     }
---
+++ Test/Integration/Plugin/Catalog/Block/Adminhtml/Product/Attribute/Edit/Tab/Front/DisplaySearchboxTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace MageSuite\ElasticSuiteAddons\Test\Integration\Plugin\Catalog\Block\Adminhtml\Product\Attribute\Edit\Tab\Front;
+
+/**
+ * @magentoDbIsolation enabled
+ * @magentoAppIsolation enabled
+ */
+class DisplaySearchboxTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var \Magento\TestFramework\ObjectManager
+     */
+    protected $objectManager;
+
+    /**
+     * @var \Magento\Eav\Model\Config
+     */
+    protected $eavConfig;
+
+    public function setUp()
+    {
+        $this->objectManager = \Magento\TestFramework\ObjectManager::getInstance();
+        $this->eavConfig = $this->objectManager->get(\Magento\Eav\Model\Config::class);
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     */
+    public function testItSetsAndReturnsDisplaySearchbox()
+    {
+        $noOption = '0';
+
+        $attribute = $this->eavConfig->getAttribute(\Magento\Catalog\Model\Product::ENTITY, 'color');
+        $attribute->setDisplaySearchbox($noOption);
+        $attribute->save();
+
+        $attribute = $this->eavConfig->getAttribute(\Magento\Catalog\Model\Product::ENTITY, 'color');
+
+        $this->assertEquals(
+            $noOption,
+            $attribute->getDisplaySearchbox()
+        );
+    }
+}
